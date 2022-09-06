985b53c95690c09170c38d8c509e66e3e15bef51
Anik-hasan07 <iamakashcse@gmail.com>
2022-09-06 18:11:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import About from "./components/About";
+import AddBook from "./components/AddBook";
+import BookDetail from "./components/Book/BookDetail";
+import Books from "./components/Book/Books";
+import Header from './components/Header';
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <BrowserRouter>
+      <Header/>
+    <Routes>
+     
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/add" element={<AddBook/>} />
+          <Route path="/books" element={<Books/>} />
+          <Route path="/books/:id" element={<BookDetail/>} />
+        
+        
+    </Routes>
+  </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+//1:33:05
